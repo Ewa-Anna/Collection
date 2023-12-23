63c7c86d0f695c8e41dd905a593075d1d4544040
Ewa-Anna <ewaanna073@gmail.com>
2023-12-23 12:10:28 +0100

    Updated currency exchanger with display drop-down

--- Currency-exchanger/script.js
+++ Currency-exchanger/script.js
@@ -1,6 +1,9 @@
 const currencies = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
+const exchangeRatesApi = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+
 const selectFromCurrency = document.getElementById("fromCurrencies");
 const selectToCurrency = document.getElementById("toCurrencies");
+const displayElement = document.getElementById("display");
 
 async function populateDropDown() {
     try {
@@ -13,19 +16,50 @@ async function populateDropDown() {
         for (const key in currenciesData) {
             if (currenciesData.hasOwnProperty(key)) {
                 const optionFrom = document.createElement("option");
-                optionFrom.value = currenciesData[key];
+                optionFrom.value = key;
                 optionFrom.text = `${key} (${currenciesData[key]})`;
                 selectFromCurrency.appendChild(optionFrom);
 
                 const optionTo = document.createElement("option");
-                optionTo.value = currenciesData[key];
+                optionTo.value = key
                 optionTo.text = `${key} (${currenciesData[key]})`;
                 selectToCurrency.appendChild(optionTo);
             }
         }
+        
+        selectFromCurrency.addEventListener("change", updateDisplay);
+        selectToCurrency.addEventListener("change", updateDisplay);
+
+        updateDisplay();
+
     } catch (error) {
-        console.error('Issue with fetching JSON data: ', error)
+        console.error("Issue with fetching data: ", error)
     }
 }
+
+async function fetchExchangeRates(baseCurrency) {
+    try {
+        const response = await fetch(`${exchangeRatesApi}/${baseCurrency.toLowerCase()}.json`);
+        const exchangeRatesData = await response.json();
+        return exchangeRatesData;
+    } catch (error) {
+        console.error("Issue with fetching exchange rates: ", error);
+    }
+}
+
+async function updateDisplay() {
+    const fromCurrency = selectFromCurrency.value;
+    const toCurrency = selectToCurrency.value;
+    const exchangeRatesData = await fetchExchangeRates(fromCurrency);
+    console.log(exchangeRatesData[fromCurrency])
+
+    if (exchangeRatesData) {
+        const conversionRate = exchangeRatesData[toCurrency];
+        const amount = 1;
+        const convertedAmount = amount * conversionRate;
+        displayElement.textContent = `${amount} ${fromCurrency} is ${convertedAmount.toFixed(2)} ${toCurrency}`;
+    }
+}
+
 
 populateDropDown();
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This repository contains collection of small JavaScript projects.
 
 [Preview](https://ewa-anna.github.io/Collection/)
 
-- [Voice Recognition App](Voice-recognition-app/index.html) [Code](https://github.com/Ewa-Anna/Collection/tree/main/Voice-recognition-app)
-- [Currency exchanger](Currency-exchanger/index.html) [Code](https://github.com/Ewa-Anna/Collection/tree/main/Currency-exchanger)
+- [Voice Recognition App](Voice-recognition-app/index.html) - [Code](https://github.com/Ewa-Anna/Collection/tree/main/Voice-recognition-app)
+- [Currency exchanger](Currency-exchanger/index.html) - [Code](https://github.com/Ewa-Anna/Collection/tree/main/Currency-exchanger)
 
 
-## Acknowledgement
+## Acknowledgements
 - Currency exchanger [fawazahmed0's currency api](https://github.com/fawazahmed0/currency-api)
